a52d63c853bfbe1e1121cfcf1dcb52a76d849aca
Dostonbek Usmonov <usmonovdoston48@gmail.com>
2025-07-01 09:26:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from telethon import TelegramClient, events
 from config import Config
 from handlers import *
 import logging
+import asyncio
 
 logging.basicConfig(format='[%(levelname)s] %(message)s', level=logging.INFO)
 
+client = TelegramClient('bot', Config.API_ID, Config.API_HASH)
+
 async def main():
-    client = TelegramClient('bot', Config.API_ID, Config.API_HASH).start(bot_token=Config.BOT_TOKEN)
-    
+    await client.start(bot_token=Config.BOT_TOKEN)
+
     client.add_event_handler(handle_start, events.NewMessage(pattern='/start'))
     client.add_event_handler(handle_help, events.NewMessage(pattern='/help'))
     client.add_event_handler(handle_about, events.NewMessage(pattern='/about'))
@@ -16,10 +19,9 @@ async def main():
     client.add_event_handler(handle_profile_pic, events.NewMessage(pattern=r'/(dp|profile_pic)\s*'))
     client.add_event_handler(handle_download, events.NewMessage())
     client.add_event_handler(handle_callback, events.CallbackQuery())
-    
-    logging.info("Bot started!")
+
+    logging.info("🤖 Bot ishga tushdi!")
     await client.run_until_disconnected()
 
 if __name__ == '__main__':
-    import asyncio
     asyncio.run(main())
